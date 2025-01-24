d59ac9c8992653025386ca323fa0569b1068a18c
Kim na young <kimna041208@gmail.com>
2025-01-24 14:21:55 +0900

    2주차-1

---
+++ src/main/java/org/example/Heap/Heap7662.java
@@ -0,0 +1,82 @@
+package org.example.Heap;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.HashMap;
+
+public class Heap7662 {
+  static PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+  static PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+  static HashMap<Integer, Integer> map = new HashMap<>();
+
+  static void addNum(int n) {
+    minHeap.add(n);
+    maxHeap.add(n);
+    map.put(n, map.getOrDefault(n, 0) + 1);
+  }
+
+  static void removeNum(int n) {
+    if (n == 1) {
+      cleanHeap(maxHeap);
+      if (!maxHeap.isEmpty()) {
+        int maxValue = maxHeap.poll();
+        map.put(maxValue, map.get(maxValue) - 1);
+      }
+    }
+    else if (n == -1) {
+      cleanHeap(minHeap);
+      if (!minHeap.isEmpty()) {
+        int minValue = minHeap.poll();
+        map.put(minValue, map.get(minValue) - 1);
+      }
+    }
+  }
+
+  static void cleanHeap(PriorityQueue<Integer> heap) {
+    while (!heap.isEmpty() && map.getOrDefault(heap.peek(), 0) == 0) {
+      heap.poll();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int T = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < T; i++) {
+      int k = Integer.parseInt(br.readLine());
+      minHeap.clear();
+      maxHeap.clear();
+      map.clear();
+
+      for (int j = 0; j < k; j++) {
+        String[] input = br.readLine().split(" ");
+        String op = input[0];
+        int n = Integer.parseInt(input[1]);
+
+        if (op.equals("I"))
+          addNum(n);
+        else if (op.equals("D"))
+          removeNum(n);
+      }
+
+      cleanHeap(minHeap);
+      cleanHeap(maxHeap);
+
+      if (minHeap.isEmpty() || maxHeap.isEmpty())
+        bw.write("EMPTY\n");
+      else
+        bw.write(maxHeap.peek() + " " + minHeap.peek() + "\n");
+    }
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
---
+++ src/main/java/org/example/Recursion/Recursion2851.java
@@ -0,0 +1,34 @@
+package org.example.Recursion;
+
+import java.util.Scanner;
+
+public class Recursion2851 {
+  static int sum = 0;
+  static int min = 100;
+
+  static int superMario(int[] arr, int i) {
+    if (i == arr.length)
+      return sum;
+    if (Math.abs(sum + arr[i] - 100) > min) {
+      min = Math.abs(sum + arr[i] - 100);
+      return sum;
+    }
+
+    sum += arr[i];
+    min = Math.min(min, Math.abs(sum - 100));
+
+    return superMario(arr, i + 1);
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int[] arr = new int[10];
+
+    for (int i = 0; i < 10; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    System.out.println(superMario(arr, 0));
+  }
+}
